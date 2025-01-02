e471de1495c45ea4b2dcc5a384607811263e08be
ERoydev <e.roydev@gmail.com>
2025-01-02 21:29:51 +0200

    updates

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource
-from .services import get_gas_price, get_gas_prediction
+from .services import get_gas_price, get_gas_prediction, oracle_interaction
 
 class GasPrice(Resource):
     def get(self):
@@ -9,4 +9,9 @@ class GasPrice(Resource):
 class GasPrediction(Resource):
     def get(self):
         prediction = get_gas_prediction()  # Call the function to predict gas fee windows
+        return prediction
+
+class GetOracle(Resource):
+    def get(self):
+        prediction = oracle_interaction()
         return prediction
